8f812ada9b66a9083b9882a6bc51aa71dc05a410
eternalcode12 <eternalcode@163.com>
2021-06-15 14:51:17 +0800

    add koa-router middleware

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const koa = require('koa2')
 const cors = require('koa2-cors')
+const router = require('koa-router')()
 
 const app = new koa()
 
 // 配置跨域(所有的中间件都有顺序，跨域配置应该放在最上面)
 app.use(cors())
-app.use(async ctx => {
+app.use(router.routes(), router.allowedMethods())
+
+router.get("/", async ctx => {
   ctx.body = {
     code: 200,
-    msg: 'hello'
+    msg: 'hello world'
   }
 })
 
 app.listen(3000, () => {
-  console.log("[demo]: server is running at localhost:3000.")
+  console.log("[demo]: server is running at http://localhost:3000.")
 })
