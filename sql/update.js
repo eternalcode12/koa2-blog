b7d7e4b3e23a2e5139130e9df6e7a2c0cef69100
eternalcode12 <eternalcode@163.com>
2021-06-15 18:17:54 +0800

    update: add library for formatting time "moment"

--- sql/update.js
+++ sql/update.js
@@ -1,10 +1,13 @@
 const user = require('../models/user')
+const moment = require('moment')
 
 const userUpdate = (username, password, phone) => {
   user.update({
     username,
     password,
-    phone
+    phone,
+    // 这里在更新特殊字段时，必须加上引号
+    'updatedAt': moment().format("YYYY-MM-DD HH:mm:ss")
   }, {
     where: {
       phone
