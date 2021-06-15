a2392672e8e8a3d0118136917a02c16b83ed3725
eternalcode12 <eternalcode@163.com>
2021-06-15 14:56:34 +0800

    try to separate the routing configuration to avoid a bloated project

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const koa = require('koa2')
 const cors = require('koa2-cors')
-const router = require('koa-router')()
+const router = require('./routes/index')
 
 const app = new koa()
 
 // 配置跨域(所有的中间件都有顺序，跨域配置应该放在最上面)
 app.use(cors())
 app.use(router.routes(), router.allowedMethods())
-
-router.get("/", async ctx => {
-  ctx.body = {
-    code: 200,
-    msg: 'hello world'
-  }
-})
 
 app.listen(3000, () => {
   console.log("[demo]: server is running at http://localhost:3000.")
