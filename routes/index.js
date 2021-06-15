4ea942d7aac6dd742288b6382725079661cf241d
eternalcode12 <eternalcode@163.com>
2021-06-15 15:41:56 +0800

    update: use sequelize to create a user and return the message.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const users = require('../models/user')
 
 router.prefix('/api')
 
@@ -11,11 +12,29 @@ router
   })
 
   .post('/register', async ctx => {
-    ctx.body = {
-      code: 200,
-      msg: 'register',
-      // 这里 ctx.request.body 可以解析页面传回的数据
-      data: ctx.request.body
+    // 在这里开始写注册内容
+    console.log(ctx.request.body)
+    let {
+      username,
+      password
+    } = ctx.request.body
+    // 这里对于业务的一个判断执行对应的一个操作
+    if (username !== '' && password !== '') {
+      users.create({
+        username,
+        password
+      })
+      ctx.body = {
+        code: 200,
+        msg: 'success',
+        // 这里 ctx.request.body 可以解析页面传回的数据
+        // data: ctx.request.body
+      }
+    } else {
+      ctx.body = {
+        code: 400,
+        msg: 'fail'
+      }
     }
   })
 
