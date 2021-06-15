01d7fcca98ef5eb8a0ea7488f065be6be299bfff
eternalcode12 <eternalcode@163.com>
2021-06-15 17:57:23 +0800

    update: add update user info api

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
 const router = require('koa-router')()
 // 引入对应的控制器
 const user = require('../controller/user')
-// const user = require('../models/user')
 
 router.prefix('/api')
 
 router
-  .get('/', async ctx => {
-    ctx.body = {
-      code: 200,
-      msg: 'home'
-    }
-  })
-
   .post('/user/register', user.apiRegister)
+
+  .put('/user/update', user.apiUpdateUserInfo)
 
 module.exports = router
